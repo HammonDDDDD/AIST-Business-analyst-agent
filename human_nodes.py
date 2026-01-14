f45536881ec18423f802f6baf41c5dc3f2172ef9
Tecevik <temajanket@gmail.com>
2026-01-14 20:08:15 +0300

    убраны недочёты

--- human_nodes.py
+++ human_nodes.py
@@ -1,12 +1,3 @@
-"""
-TODO:
-1. Рендер ProjectArtifact в красивый Markdown
-2. Вывести Markdown в консоль/веб/Telegram
-3. Прочитать input от пользователя
-4. Обновить state на основе input
-"""
-
-
 def human_node(state):
     artifact = state.get('draft_artifact', {})
     print("\nНазвание:", artifact.get('title', 'N/A'))
